5faf2a2c8125b2d8abd55f33a881d3959291a2b1
Nick Liggett <nick.liggett@gmail.com>
2023-01-19 13:54:12 -0700

    Attempt to import font

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import satisfied from "./assets/fonts/Satisfy-Regular.ttf"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleText}>Leaf It To Me</Text>
     </View>
   );
 }
@@ -13,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F2E7BB',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    color: '#08BA46',
+    fontSize: 50,
+    fontFamily: "Satisfy-Regular",
+  }
 });
